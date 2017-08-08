e3fb61e27656807e6a38ab21dbd9462e11d1348f
hitzkrieg <hitesh1994golchha@gmail.com>
2017-08-08 13:32:37 +0530

    Changed the method cluster_mentions in clustering_common.py because it was erroneous (haven't figured yet why) and restored it to the original

--- src/baseline_system/clustering_common.py
+++ src/baseline_system/clustering_common.py
@@ -3,13 +3,43 @@ Utility script for clustering functions
 """
 
 
-def cluster_mentions(mention_list, score, clusters = [], merge_initially= False):
+def cluster_mentions(mention_list, score):
     """
     Cluster the predicate mentions in a greedy way: assign each predicate to the first
     cluster with similarity score > 0.5. If no such cluster exists, start a new one.
     :param mention_list: the mentions to cluster
     :param score: the score function that receives a mention and a cluster and returns a score
+    :return: clusters of mentions
+    """
+    clusters = []
+
+    for mention in mention_list:
+        found_cluster = False
+        for cluster in clusters:
+            if score(mention, cluster) > 0.5:
+                cluster.add(mention)
+                found_cluster = True
+                break
+
+        if not found_cluster:
+            clusters.append(set([mention]))
+
+    return clusters
+
+
+
+
+
+def cluster_mentions_changed(mention_list, score, clusters = [], merge_initially= False):
+    """
+    Cluster the predicate mentions in a greedy way: assign each predicate to the first
+    cluster with similarity score > 0.5. If no such cluster exists, start a new one.
+    This method was used 
+    :param mention_list: the mentions to cluster
+    :param score: the score function that receives a mention and a cluster and returns a score
     :param clusters: the initial clusters received by the algorithm from a previous coreference pipeline
+    :param merge_initially: Boolean value which indicates that initialised clusters needed to be merged before further clustering.
+    This method was used for doing entity coreference from already initialized clusters incoming from corenlp (intra document).
     :return: clusters of mentions
     """
 
